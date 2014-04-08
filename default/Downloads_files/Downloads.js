66ed6b256e0ae55f77699f75e15b4685ecfb2d5a
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-04-08 14:58:43 +0200

    Import legacy files for my site as published by iWeb.

---
+++ default/Downloads_files/Downloads.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20140408
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,13),url:'Downloads_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Downloads_files/stroke_1.png'},{rect:new IWRect(1,-1,798,2),url:'Downloads_files/stroke_2.png'},{rect:new IWRect(799,-1,2,2),url:'Downloads_files/stroke_3.png'},{rect:new IWRect(799,1,2,13),url:'Downloads_files/stroke_4.png'},{rect:new IWRect(799,14,2,2),url:'Downloads_files/stroke_5.png'},{rect:new IWRect(1,14,798,2),url:'Downloads_files/stroke_6.png'},{rect:new IWRect(-1,14,2,2),url:'Downloads_files/stroke_7.png'}],new IWSize(800,15)),shadow_0:new IWShadow({blurRadius:5,offset:new IWPoint(0.7071,0.7071),color:'#333333',opacity:0.750000}),shadow_1:new IWShadow({blurRadius:5,offset:new IWPoint(0.7071,0.7071),color:'#333333',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Downloads_files/DownloadsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
