d2624f55380f556ec0adcf0f978620f845efda24
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-06-04 23:16:03 +0200

    Add main.js for application-wide functionality.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//____________________________________________________________________
+// File: main.js
+//____________________________________________________________________
+//
+// Author: Shaun Ashby <shaun@ashby.ch>
+// Created: 2014-06-04 16:08:36+0200
+// Revision: $Id$
+// Description: This is the main JavaScript file for www.ashby.ch.
+//
+// Copyright (C) 2014 Shaun Ashby
+//
+//
+//--------------------------------------------------------------------
+
+(function($){
+	"use script";
+	$("#wip_button").click(function(){
+		alert("This button isn't active yet. All it does is show this lousy popup....");
+	});
+})(jQuery);
